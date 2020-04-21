a3401d3518376199e3b745255f3f495663be3f7d
Alex Lampert <55845056+lamps24@users.noreply.github.com>
2020-04-21 14:10:55 -0500

    Add files via upload

---
+++ value_function/ipw.R
@@ -0,0 +1,47 @@
+########################################################################
+# Inverse Probability Weights (IPW)
+# Compute value using the IPW estimator
+#
+# inputs:
+#   - data = data
+#   - i = treatment policy that's being tested
+#         272x1 vector of 8 levels, turned into:
+#         272x8 matrix of dummies indicating which treatment each patient will receive
+# outputs:
+#   - value, a scalar
+#
+########################################################################
+
+library(nnet)
+library(fastDummies)
+
+ipw = function(df, i)
+{
+  
+  # extract only the variables needed for fitting so we can tell it to use all vars  
+  df_for_fit = df[, 8:ncol(df)]
+  
+  # fit model
+  pi.h = multinom(treatment ~ ., data=df_for_fit)
+  
+  # compute the fitted values (propensity scores) - each patient has 8 probs
+  pscores = predict(pi.h, type="probs")
+  
+  # turn treatment and regimen factors into 8 dummies - needs to be numeric
+  original_treatment = as.numeric(as.matrix(dummy_cols(df$treatment))[, 2:5])
+  original_treatment = matrix(original_treatment, nrow=272, ncol=4)
+  
+  new_treatment = as.numeric(as.matrix(dummy_cols(i))[, 2:5])
+  new_treatment = matrix(new_treatment, nrow=272, ncol=4)
+  
+  # compute pi.d - probability original treatment = new treatment
+  # pscore corresponding to new treatment
+  pi.d = rowSums(pscores * new_treatment)
+  
+  # compute c.d - indicator for whether treatment = regimen
+  c.d = rowSums(original_treatment * new_treatment)
+  
+  # treatment effect
+  value = mean((df$eventdeath * c.d) / pi.d)
+  return(list(value=value))
+}
