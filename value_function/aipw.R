f7484e2ae5a3a51ff5e9b2e69e141cae8209f363
Alex Lampert <55845056+lamps24@users.noreply.github.com>
2020-04-24 12:43:20 -0500

    fixed input data for line 39 so that i is a factor for the predict

--- value_function/aipw.R
+++ value_function/aipw.R
@@ -14,9 +14,9 @@ aipw = function(df, eta)
 {
   
   # convert eta values to policies - can adjust form of policy
-  chem = ifelse(df$var1 < eta[1], 1, 0) 
-  amp = ifelse(df$timerecurrence < eta[2], 1, 0)
-  policy = rep(0,272)
+  chem = ifelse(df$age < eta[1], 1, 0) 
+  amp = ifelse(df$diam < eta[2], 1, 0)
+  policy = rep(0, 272)
   policy = ifelse(chem == 0 & amp == 0, 1, policy)
   policy = ifelse(chem == 0 & amp == 1, 2, policy)
   policy = ifelse(chem == 1 & amp == 0, 3, policy)
@@ -24,7 +24,7 @@ aipw = function(df, eta)
   i = policy
   
   # ipw estimate
-  ipw = ipw(df, i)
+  ipw = ipw(df, eta)
   ipw.value = ipw$value
   pi.d = ipw$pi.d
   c.d = ipw$c.d
@@ -35,7 +35,7 @@ aipw = function(df, eta)
   
   # create new dataframe to be used for generating predictions based on new treatment decision i
   data.policy = df
-  data.policy$treatment = i # overwrites the treatment variable with new treatment decision
+  data.policy$treatment = as.factor(i) # overwrites the treatment variable with new treatment decision
   or.preds = predict(q.or, newdata=data.policy, type="response")
   
   # adjustment
