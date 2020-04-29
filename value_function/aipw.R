7e3f0e33a671a7a40631a9286b24bffa8df07074
Alex Lampert <55845056+lamps24@users.noreply.github.com>
2020-04-29 14:51:22 -0500

    Update aipw.R

--- value_function/aipw.R
+++ value_function/aipw.R
@@ -5,26 +5,24 @@
 # inputs:
 #   - data = data
 #   - eta = vector of eta values that will form policy
+#   - trt = column number of treatment being considered 
+#         Probably:
+#           5: chemo
+#           6: hormonal
+#           7: amputation
 # outputs:
 #   - value, a scalar
 #
 ########################################################################
 
-aipw = function(df, eta)
+aipw = function(df, eta, trt)
 {
   
   # convert eta values to policies - can adjust form of policy
-  chem = ifelse(df$age < eta[1], 1, 0) 
-  amp = ifelse(df$diam < eta[2], 1, 0)
-  policy = rep(0, 272)
-  policy = ifelse(chem == 0 & amp == 0, 1, policy)
-  policy = ifelse(chem == 0 & amp == 1, 2, policy)
-  policy = ifelse(chem == 1 & amp == 0, 3, policy)
-  policy = ifelse(chem == 1 & amp == 1, 4, policy)
-  i = policy
+  i = ifelse(df$age < eta[1] & df$diam > eta[2] & df$grade > eta[3], 1, 0) 
   
   # ipw estimate
-  ipw = ipw(df, eta)
+  ipw = ipw(df, eta, trt)
   ipw.value = ipw$value
   pi.d = ipw$pi.d
   c.d = ipw$c.d
@@ -33,9 +31,9 @@ aipw = function(df, eta)
   df_for_fit = df[, c(2, 8:ncol(df))]
   q.or = glm(eventdeath ~ ., data=df_for_fit, family=binomial)
   
-  # create new dataframe to be used for generating predictions based on new treatment decision i
+  # predictions with new policy (replace original treatment with new)
   data.policy = df
-  data.policy$treatment = as.factor(i) # overwrites the treatment variable with new treatment decision
+  data.policy[, trt] = i
   or.preds = predict(q.or, newdata=data.policy, type="response")
   
   # adjustment
@@ -43,5 +41,6 @@ aipw = function(df, eta)
   
   # compile estimate
   value = ipw.value - adjustment
-  return(list(value=value))
+  return(list(value=value, ipw.value=ipw.value, adjustment=adjustment))
+  
 }
