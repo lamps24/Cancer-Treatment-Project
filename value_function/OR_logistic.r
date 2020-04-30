281b3a8ce59df99afd6db6e5ae23c675fe50ef15
OudomKour <63416318+OudomKour@users.noreply.github.com>
2020-04-30 16:41:58 -0500

    updated arguments 'trt' and 'i'

--- value_function/OR_logistic.r
+++ value_function/OR_logistic.r
@@ -4,8 +4,10 @@
 #
 # inputs:
 #   - data = dataset
-#   - eta = values of eta for the policy for chemo and amputation
-#      - currently has length 2, but can be changed depending on how we structure policy
+#   - trt = a scalar for which treatment is being used 
+#      - chemo is column 5, and amputation is column 7
+#   - i = policy vector, supplied from genetic algorithm
+#
 # outputs:
 #   - deltaOR_log, the resulting value (scalar)
 #
@@ -13,54 +15,40 @@
 
 library("dplyr")
 
-OR_log = function(df, eta){
+OR_log = function(df, trt, i){
+  
   # Assign inputs to different var names
-  newdat = df
-  newdat = select(newdat, -c(chemo, amputation, hormonal)) # remove the treatment columns
-  N = length(newdat$age)
+  newdat = df[, c(2, trt, 8:ncol(df))] # remove the treatment columns, extra response columns
+  N = nrow(newdat)
   
-  ##########################################################
-  # Convert eta values to policies
-  # Can be modified to accept several values for eta
-  # Right now there is only 2, one for predicting chemo and one for amputation
+  # Q function if treatment is chemo 
+  # Currently there are a few different options of models for how to best include interactions with treatment
+  if (trt == 5){
+    #mod = glm(eventdeath ~ .*chemo, data = newdat, family = "binomial")
+    #mod = glm(eventdeath ~ chemo + (histtype + grade + angioinv + lymphinfil + age + posnodes + diam)*chemo, data = newdat, family = "binomial")
+    mod = glm(eventdeath ~ chemo + (histtype + grade + angioinv + lymphinfil + age + posnodes + diam+NM_012067
+                                    + NM_021069 +NM_005132 +AL117638 + NM_006398+NM_004950 +NM_006461 +Contig23211_RC+NM_016109
+                                    +Contig42011_RC+NM_001085 +NM_001109+NM_002274)*chemo , data = newdat, family = 'binomial')
+    #mod = glm(eventdeath ~ chemo+(histtype + grade + angioinv + lymphinfil + age + posnodes + diam+ NM_001627+NM_012067+Contig21225_RC+NM_003430+NM_021069+NM_005132+AL117638+NM_006398+NM_004950+Contig23211_RC+NM_016359+L27560+NM_001109+M24895)*chemo, data = newdat, family = "binomial")
+  }
   
-  chem = ifelse(newdat$age < eta[1], 1, 0) 
-  amp = ifelse(newdat$timerecurrence < eta[2], 1, 0)
-  
-  policy = rep(0,272)
-  
-  policy = ifelse(chem == 0 & amp == 0, 1, policy)
-  policy = ifelse(chem == 0 & amp == 1, 2, policy)
-  policy = ifelse(chem == 1 & amp == 0, 3, policy)
-  policy = ifelse(chem == 1 & amp == 1, 4, policy)
-  A = policy
-  
-  #########################################################
-  
-  # Create model for the betas, can be modified in the future.
-  mod = glm(eventdeath ~ ., data = newdat, family = "binomial")
-  
-  # Instantiate new matrices for each treatment type
-  X1 = newdat
-  X1$treatment = as.factor(1)
-  X2 = newdat
-  X2$treatment = as.factor(2)
-  X3 = newdat
-  X3$treatment = as.factor(3)
-  X4 = newdat
-  X4$treatment = as.factor(4)
-  
-  # Will multiply the Q vector by 1 depending on the treatment policy, 0 if not
-  vec1 = ifelse(A == 1, 1, 0)
-  Q1 = predict(mod, X1) * vec1
-  vec2 = ifelse(A == 2, 1, 0)
-  Q2 = predict(mod, X2) * vec2 
-  vec3 = ifelse(A == 3, 1, 0)
-  Q3 = predict(mod, X3) * vec3
-  vec4 = ifelse(A == 4, 1, 0)
-  Q4 = predict(mod, X4) * vec4
-  
-  deltaOR_log = sum(Q1 + Q2 + Q3 + Q4) / N
+  # Q function if treatment is amputation 
+  if (trt ==7){
+    mod = glm(eventdeath ~ .*amputation, data = newdat, family = "binomial", control = list(maxit = 50)) # without control, says algorithm doesn't converge
+    #mod = glm(eventdeath ~ amputation + (histtype + grade + angioinv + lymphinfil + age + posnodes + diam+ NM_001627+NM_012067+Contig21225_RC+NM_003430+NM_021069+NM_005132+AL117638+NM_006398+NM_004950+Contig23211_RC+NM_016359+L27560+NM_001109+M24895+NM_002274)*amputation, data = newdat, family = "binomial")
+  }
+
+  # Instantiate new matrices for each treatment
+  X0 = df
+  X0[trt] = 0
+  X1 = df
+  X1[trt] = 1
+
+  # Will multiply the predictions with the policy vector i
+  Q0 = predict(mod, X0, type = 'response') * (1-i)
+  Q1 = predict(mod, X1, type = 'response') * i 
+
+  value = sum(Q0 + Q1) / N
   
   return(deltaOR_log)
 }
