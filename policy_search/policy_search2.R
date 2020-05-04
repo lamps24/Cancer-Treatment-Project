19adb0b37cad4becdfb51e6274e13948341f8248
OudomKour <63416318+OudomKour@users.noreply.github.com>
2020-05-04 15:42:39 -0500

    can now run more than one generation

--- policy_search/policy_search2.R
+++ policy_search/policy_search2.R
@@ -32,7 +32,7 @@
 library(data.table)
 library(MASS)
 
-gen_alg2 = function(df, trt, M=100, u=0.3, lam=3, gen=1, val_fun=OR_log, var_list=c(9, 10, 12, 13, 18, 21, 25)) {
+gen_alg = function(df, trt, M=100, u=0.3, lam=3, gen=1, val_fun=OR_log, var_list=c(9, 10, 12, 13, 18, 21, 25)) {
   
   # initialize values at means
   p = length(var_list)
@@ -81,34 +81,35 @@ gen_alg2 = function(df, trt, M=100, u=0.3, lam=3, gen=1, val_fun=OR_log, var_lis
     for (r in 1:M) {
       for(s in 1:offspring[r]) {
         if (rnorm(1) < u & s>1) {
-          Z[l, ] = eta[r, ] + 20*rnorm(p) # can change how to mutate offspring
+          Z[l, ] = eta[r, ] * 0.98 # can change how to mutate offspring
         }
         else {
           Z[l, ] = eta[r, ]
         }
         
         # convert eta values to policies - can adjust form of policy
-        i = ifelse(df[, var_list[1]] < eta[j, 1] & 
-                     df[, var_list[2]] > eta[j, 2] & 
-                     df[, var_list[3]] > eta[j, 3] &
-                     df[, var_list[4]] > eta[j, 4] &
-                     df[, var_list[5]] < eta[j, 5] &
-                     df[, var_list[6]] > eta[j, 6] &
-                     df[, var_list[7]] > eta[j, 7], 1, 0)
+        i = ifelse(df[, var_list[1]] < Z[l, 1] & 
+                     df[, var_list[2]] > Z[l, 2] & 
+                     df[, var_list[3]] > Z[l, 3] &
+                     df[, var_list[4]] > Z[l, 4] &
+                     df[, var_list[5]] < Z[l, 5] &
+                     df[, var_list[6]] > Z[l, 6] &
+                     df[, var_list[7]] > Z[l, 7], 1, 0)
         
         Vtemp[l] = val_fun(df, trt, i)$value # can change which value search function 
         l = l + 1
       }
     }
     tempvec = data.table(cbind(Vtemp, Z))
-    tempvec = tempvec[order(tempvec$V1, decreasing = TRUE)]
+    tempvec = tempvec[order(tempvec$V1, decreasing = FALSE)]
     tempvec = tempvec[1:M]
     V = as.matrix(tempvec[, 1])
-    eta = as.matrix(tempvec[, 2:p+1])
+    num_col = p + 1
+    eta = as.matrix(tempvec[, 2:num_col])
     if (V[1] < min_value){
       min_value = V[1]
       eta_best = eta[1,]
     }
   }
-  return(list(min_value=min_value, eta_best=eta_best))
+  return(c(min_value, eta_best))
 }
