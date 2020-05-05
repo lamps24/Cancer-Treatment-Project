8b80d5e7a107e807a724851ed41169de35e62d74
Alex Lampert <55845056+lamps24@users.noreply.github.com>
2020-05-05 09:09:30 -0500

    numbered function name

--- policy_search/policy_search3.r
+++ policy_search/policy_search3.r
@@ -32,7 +32,7 @@
 library(data.table)
 library(MASS)
 
-gen_alg = function(df, trt, M=100, u=0.3, lam=3, gen=1, val_fun=OR_log, var_list=c(18, 21, 25)) {
+gen_alg3 = function(df, trt, M=100, u=0.3, lam=3, gen=1, val_fun=OR_log, var_list=c(18, 21, 25)) {
   
   # initialize values at means
   p = length(var_list)
